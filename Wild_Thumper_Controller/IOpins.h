afe061e23e20aaef94728c595066ec67707ad195
Floris van Breugel <caveman@flycave.(none)>
2012-07-23 14:50:28 -0700

    wild thumper firmware - no changes

---
+++ Wild_Thumper_Controller/IOpins.h
@@ -0,0 +1,21 @@
+
+#define LmotorA             3  // Left  motor H bridge, input A
+#define LmotorB            11  // Left  motor H bridge, input B
+#define RmotorA             5  // Right motor H bridge, input A
+#define RmotorB             6  // Right motor H bridge, input B
+
+#define RCleft              0  // Digital input 0
+#define RCright             1  // Digital input 1
+
+#define S0                  2  // Servo output 00
+#define S1                  4  // Servo output 01
+#define S2                  7  // Servo output 02
+#define S3                  8  // Servo output 03
+#define S4                  9  // Servo output 04
+#define S5                 10  // Servo output 05
+#define S6                 12  // Servo output 06
+
+#define Battery             0  // Analog input 00
+#define RmotorC             6  // Analog input 06
+#define LmotorC             7  // Analog input 07
+#define Charger            13  // Low=ON High=OFF
