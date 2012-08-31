7e97efe3be5f0408e7da778fe2e7787fb6cacee4
Floris van Breugel <caveman@flycave.(none)>
2012-08-31 13:34:58 -0700

    closed loop attempt

--- tcp_to_arduino/nodes/ransac_control.py
+++ tcp_to_arduino/nodes/ransac_control.py
@@ -7,22 +7,41 @@ import time
 
 from camera_aravis.msg import FloatArray
 
+def ps3_to_pwm(val, command=None):
+
+    if command == 'speed':
+        val /= 1.
+    elif command == 'steer':
+        val /= 3.
+
+    max_val = 254
+    min_val = 127
+    
+    return int(((val+1.)/2.) * (max_val-min_val) + min_val)
+    
 
 class Arduino_Data_Client:
     def __init__ (self, rate):
         rospy.wait_for_service('send_data_to_arduino')
         self.send_data_to_arduino = rospy.ServiceProxy('send_data_to_arduino', SendDataToArduino)
         
-        self.ransac_sub = rospy.Subscriber("ransac_data",FloatArray,self.ransac_callback)
-
+        rospy.Subscriber("ransac_data",FloatArray,self.ransac_callback)
+        self.Joy = None
+        rospy.Subscriber("joy", Joy, self.save_buttons)
+        
         rospy.init_node("ransac_to_arduino")
         self.slope = 0
 
         self.main()
+    
+    def save_buttons(self, Joy):
+        if Joy != self.Joy:
+            self.Joy = Joy
                         
     def main(self):
         try:
             print 'spinning'
+            
             rospy.spin()
         except KeyboardInterrupt:
             print "Shutting Down"
@@ -37,11 +56,34 @@ class Arduino_Data_Client:
         desired_slope = -0.015
         error = self.slope - desired_slope
         
-        Kp = 0.5
+        Kp = -13
         
         control_signal = Kp*error
                     
-        print control_signal
+        #print control_signal
+        self.send_data(control_signal)
+        
+    def send_data(self, control_signal):
+        if self.Joy is not None:
+            motor_speed_ps3 = ps3_to_pwm(-1*self.Joy.axes[1], command='speed')
+        else:
+            motor_speed_ps3 = 0
+        
+        if self.Joy.axes[12] == 1:
+            time_start = time.time()
+            data = self.send_data_to_arduino("s", "mspd", motor_speed_ps3)
+            print 'communication time roundtrip: ', time.time() - time_start, 'val: ', motor_speed_ps3
+        
+        else:
+            time_start = time.time()
+            motor_speed_control = ps3_to_pwm(control_signal, command='speed')
+            if motor_speed_control < 160:
+                motor_speed_control = 160
+            if motor_speed_control > 190:
+                motor_speed_control = 190
+            data = self.send_data_to_arduino("s", "mspd", motor_speed_control)
+            print 'val: ', control_signal, motor_speed_control
+            
             
             
 
